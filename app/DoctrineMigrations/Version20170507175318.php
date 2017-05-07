7b3b8d352163ef97d59c567917c52d7090a4dd9a
majidmvulle <majid@majidmvulle.com>
2017-05-07 21:53:59 +0400

    fix relation to crawler_classifieds_model from ClassifiedsModelType

--- app/DoctrineMigrations/Version20170507175318.php
+++ app/DoctrineMigrations/Version20170507175318.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170507173639 extends AbstractMigration
+class Version20170507175318 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -19,7 +19,7 @@ class Version20170507173639 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE crawler_classifieds_model_type (id INT AUTO_INCREMENT NOT NULL, model_id INT DEFAULT NULL, trim VARCHAR(100) DEFAULT NULL, trim_source_id VARCHAR(100) DEFAULT NULL, engine SMALLINT NOT NULL, transmission VARCHAR(100) DEFAULT NULL, transmission_source_id VARCHAR(100) DEFAULT NULL, seats SMALLINT DEFAULT NULL, cylinders SMALLINT DEFAULT NULL, body_type VARCHAR(100) DEFAULT NULL, body_type_source_id VARCHAR(100) DEFAULT NULL, years LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', is_gcc TINYINT(1) DEFAULT \'1\' NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_152590297975B7E7 (model_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE crawler_classifieds_model_type ADD CONSTRAINT FK_152590297975B7E7 FOREIGN KEY (model_id) REFERENCES vehicle_model (id)');
+        $this->addSql('ALTER TABLE crawler_classifieds_model_type ADD CONSTRAINT FK_152590297975B7E7 FOREIGN KEY (model_id) REFERENCES crawler_classifieds_model (id)');
     }
 
     /**
