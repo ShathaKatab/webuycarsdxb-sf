a3fdfa769eca9368e4db3247e41e08b9edebadb8
majidmvulle <majid@majidmvulle.com>
2017-06-14 02:05:30 +0400

    add missing migration file

---
+++ app/DoctrineMigrations/Version20170613215730.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170613215730 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE appointment_details CHANGE branch branch LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:branch_object)\', CHANGE branch_timing branch_timing LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:branch_timing_object)\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE appointment_details CHANGE branch branch LONGTEXT NOT NULL COMMENT \'(DC2Type:branch_object)\', CHANGE branch_timing branch_timing LONGTEXT NOT NULL COMMENT \'(DC2Type:branch_timing_object)\'');
+    }
+}
